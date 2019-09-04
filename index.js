e5867a4e39ca1f60b604552dcfd15faff51b376c
Dave <dave@wearecrystal.uk>
2019-09-04 17:17:29 +0100

    Move index file

--- index.js
+++
@@ -1,8 +0,0 @@
-import { scheduleJob } from 'node-schedule';
-import dotenv from 'dotenv/config';
-
-const cron = process.env.cron || '30 7 * * *';
-
-scheduleJob(cron, () => {
-  console.log('I think node sucks');
-});
