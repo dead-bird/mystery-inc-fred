4cb65afc7909087e5bf2bfb9ce68bedf7a2d81e2
Dave <dave@wearecrystal.uk>
2019-09-04 13:55:18 +0100

    Node schedule

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('I think node sucks');
+import { scheduleJob } from 'node-schedule';
+import dotenv from 'dotenv/config';
+
+const cron = process.env.cron || '30 7 * * *';
+
+scheduleJob(cron, () => {
+  console.log('I think node sucks');
+});
